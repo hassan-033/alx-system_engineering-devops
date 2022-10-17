c8783a044237fe6cd3e2e1c72a426b0cdecf7aeb
hassan-033 <99290640+hassan-033@users.noreply.github.com>
2022-10-17 20:02:43 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 """
-Gathers data from an API and exports it to CSV file
-Records all tasks that are owned by this employee
-Format must be:
-"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv
+Check student .CSV output of user information
 """
+
+import csv
+import requests
+import sys
+
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+
+def user_info(id):
+    """ Check user information """
+
+    total_tasks = 0
+    response = requests.get(todos_url).json()
+    for i in response:
+        if i['userId'] == id:
+            total_tasks += 1
+
+    num_lines = 0
+    with open(str(id) + ".csv", 'r') as f:
+        for line in f:
+            if not line == '\n':
+                num_lines += 1
+
+    if total_tasks == num_lines:
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
 
 
 if __name__ == "__main__":
-    import requests
-    import sys
-    import csv
-
-    user_req = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                            format(sys.argv[1]))
-    user_name = user_req.json().get("username")
-    tasks_req = requests.get("https://jsonplaceholder.typicode.com/todos")
-    file_csv = sys.argv[1] + ".csv"
-    with open(file_csv, 'w', encoding='utf=8') as file:
-        write = csv.writer(file, delimiter=",", quoting=csv.QUOTE_ALL)
-        for each in tasks_req.json():
-            if each["userId"] == int(sys.argv[1]):
-                write.writerow([each["userId"], user_name, each["completed"],
-                                each["title"]])
+    user_info(int(sys.argv[1]))
